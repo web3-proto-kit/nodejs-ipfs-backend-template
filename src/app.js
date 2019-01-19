244280280344ddffaec65c705a680333d072ac95
Cael Rowley <caelrowley@gmail.com>
2019-01-19 15:46:50 +0000

    Removed unused code from app.js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,6 @@ import express from 'express';
 import http from 'http';
 import logger from './config/winston';
 import addRouters from './routes/app-router';
-// import * as Sentry from '@sentry/node';
-// Sentry.init({ dsn: 'https://bae974e906d64c43bcc184d8ff783832@sentry.io/1375147' });
-
-logger.info('I am a new custom info for sentry to find');
 
 const app = express();
 const server = http.createServer(app);
